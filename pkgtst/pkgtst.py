088aa094b34968e668bacd41061e100d7a81b562
Eric Lopes <nullhack@users.noreply.github.com>
2024-05-10 01:15:37 +0800

    initial commit

---
+++ pkgtst/pkgtst.py
@@ -0,0 +1,9 @@
+"""Module Docstring."""
+
+import logging
+
+logger = logging.getLogger("pkgtst")
+
+
+if __name__ == "__main__":
+    print("RUNNING!")
---
+++ tests/pkgtst_test.py
@@ -0,0 +1,8 @@
+"""Pytest test module."""
+
+
+def test_basics() -> None:
+    """A test that is always True."""
+    assert True is True
+
+
